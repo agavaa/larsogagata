f41dea21cc321ba1bbb65312b361f4f92d44df85
agata <agatajedryszek@gmail.com>
2026-03-19 19:14:37 +0100

    init page

---
+++ js/include.js
@@ -0,0 +1,7 @@
+async function loadComponent(id, file) {
+    const res = await fetch(file);
+    const text = await res.text();
+    document.getElementById(id).innerHTML = text;
+}
+
+loadComponent("header", "./components/header.html");
