7075dd46fba053f99c1ea5c4beed014c9a1bc7b3
agata <agatajedryszek@gmail.com>
2026-04-18 12:25:13 +0200

    forts

--- js/include.js
+++ js/include.js
@@ -1,3 +1,20 @@
+document.addEventListener('click', (e) => {
+    if (e.target.id === 'nav-button') {
+        const navButton = e.target;
+        console.log('test');
+        const nav = document.getElementsByTagName('nav')[0];
+        console.log(nav);
+        nav.classList.toggle('showMenu');
+        
+        if(nav.classList.contains('showMenu')) {
+            navButton.innerHTML = 'Skjul meny';
+        }
+        else {
+            navButton.innerHTML = 'Vis meny';
+        }
+    }
+});
+
 async function loadComponent(id, file) {
     const res = await fetch(file);
     const text = await res.text();
